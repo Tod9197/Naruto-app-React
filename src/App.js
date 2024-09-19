87b529043f60ae7986dd1ebe8fad8ea935fc097d
Tod9197 <dancein18@yahoo.co.jp>
2024-09-19 21:05:37 +0900

    NARUTOアプリ実装完了

--- src/App.js
+++ src/App.js
@@ -1,24 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
 
+const limit = 15;
 function App() {
+  const [characters, setCharacters] = useState([]);
+  const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+  useEffect(() => {
+    fecthCharacters();
+  }, []);
+
+  const fecthCharacters = async (page) => {
+    const apiUrl = "https://narutodb.xyz/api/character";
+    setIsLoading(true);
+    const result = await axios.get(apiUrl, { params: { page: page } });
+    console.log(result.data.characters);
+    setCharacters(result.data.characters);
+    setIsLoading(false);
+  };
+
+  const handleNext = async () => {
+    const nextPage = page + 1;
+    await fecthCharacters(nextPage);
+    setPage(nextPage);
+  };
+
+  const handlePrev = async () => {
+    const prevPage = page - 1;
+    await fecthCharacters(prevPage);
+    setPage(prevPage);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        {isLoading ? (
+          <div>Now loading...</div>
+        ) : (
+          <main>
+            <div className="cards-container">
+              {characters.map((character) => {
+                return (
+                  <div className="card" key={character.id}>
+                    <img
+                      src={
+                        character.images[0] != null
+                          ? character.images[0]
+                          : "dummy.png"
+                      }
+                      alt="character"
+                      className="card-image"
+                    />
+                  </div>
+                );
+              })}
+            </div>
+            <div className="pager">
+              <button
+                disabled={page === 1}
+                className="prev"
+                onClick={handlePrev}
+              >
+                Prev
+              </button>
+              <span className="page-number">1</span>
+              <button
+                disabled={limit > characters.length}
+                className="next"
+                onClick={handleNext}
+              >
+                Next
+              </button>
+            </div>
+          </main>
+        )}
+      </div>
+    </>
   );
 }
 
